bf3ed6efb25ded0d26c3782e7d71231257bb2f6b
Komefai <komefai@users.noreply.github.com>
2017-09-01 19:41:26 +0000

    release v0.2.0

--- PS4Macro/Properties/AssemblyInfo.cs
+++ PS4Macro/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PS4 Macro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automation utility for PS4 Remote Play")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Komefai")]
 [assembly: AssemblyProduct("PS4 Macro")]
 [assembly: AssemblyCopyright("Copyright ©  2017 Komefai")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
